152f673461b871af5dba05046224241ba42824fd
Keshavaram <keshavjayanth1401@gmail.com>
2022-06-27 12:51:33 +0530

    added back btn function

--- Blaash_andriod_sdk/src/main/java/blaash/gaming/mobile/sdk/WebViewActivity.java
+++ Blaash_andriod_sdk/src/main/java/blaash/gaming/mobile/sdk/WebViewActivity.java
@@ -3,6 +3,7 @@ package blaash.gaming.mobile.sdk;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.Button;
 
 public class WebViewActivity extends Activity {
 
@@ -14,5 +15,8 @@ public class WebViewActivity extends Activity {
         WebView webView = findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://www.codechef.com");
+
+        Button back = findViewById(R.id.button);
+        back.setOnClickListener(v -> finish());
     }
 }
