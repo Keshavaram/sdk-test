038042a29ec849bd383b09a75716cfe86fc2a373
Keshavaram <keshavjayanth1401@gmail.com>
2022-06-27 11:50:33 +0530

    webView added

---
+++ Blaash_andriod_sdk/src/main/java/blaash/gaming/mobile/sdk/WebViewActivity.java
@@ -0,0 +1,20 @@
+package blaash.gaming.mobile.sdk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.codechef.com");
+    }
+}
